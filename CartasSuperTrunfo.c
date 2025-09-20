ccf33bffd2529c9bb1c6ee6f11d3107c2e47b572
DAVIKAP <davikaptzki@yahoo.com.br>
2025-09-20 13:54:47 -0300

    Revisão com Desafio Aventureiro_CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,6 +14,10 @@ int main() {
     double Área1, Área2 = 123456.09;
     double  PIB1, PIB2 = 987654.62;
     int Turismo1, Turismo2 = 32;
+    double Densidade1 = (double) População1 / Área1;
+    double Densidade2 = (double) População2 / Área2;
+    double PIBCapita1 = (double) PIB1 / População1;
+    double PIBCapita2 = (double) PIB2 / População2;
   
   // Área para entrada de dados
 
@@ -37,11 +41,11 @@ int main() {
 
     printf("Digite a Área: ", Área1);
     scanf("%lf", &Área1);
-    printf("A Área digitada foi: %f km2\n", Área1);
+    printf("A Área digitada foi: %lf km2\n", Área1);
 
     printf("Digite o PIB: ", PIB1);
     scanf("%lf", &PIB1);
-    printf("O PIB digitado foi: R$ %f\n", PIB1);
+    printf("O PIB digitado foi: R$ %lf\n", PIB1);
 
     printf("Digite a quantidade de pontos turísticos: ", Turismo1);
     scanf("%d", &Turismo1);
@@ -66,11 +70,11 @@ int main() {
 
     printf("Digite a Área: ", Área2);
     scanf("%lf", &Área2);
-    printf("A Área digitada foi: %f km2\n", Área2);
+    printf("A Área digitada foi: %lf km2\n", Área2);
 
     printf("Digite o PIB: ", PIB2);
     scanf("%lf", &PIB2);
-    printf("O PIB digitado foi: R$ %f\n", PIB2);
+    printf("O PIB digitado foi: R$ %lf\n", PIB2);
 
     printf("Digite a quantidade de pontos turísticos: ", Turismo2);
     scanf("%d", &Turismo2);
@@ -88,11 +92,15 @@ int main() {
 
     printf("População: %d\n", População1);
 
-    printf("Área: %f km2\n", Área1);
+    printf("Área: %lf km2\n", Área1);
 
-    printf("PIB: R$ %f\n", PIB1);
+    printf("PIB: R$ %lf\n", PIB1);
     
-    printf("Número de pontos turísticos: %d\n\n", Turismo1);
+    printf("Número de pontos turísticos: %d\n", Turismo1);
+
+    printf("Densidade populacional: %lf\n", Densidade1);
+
+    printf("PIB per capita: %lf\n\n", PIBCapita1);
 
     printf("Carta 2: \n");
 
@@ -104,11 +112,15 @@ int main() {
 
     printf("População: %d\n", População2);
 
-    printf("Área: %f km2\n", Área2);
+    printf("Área: %lf km2\n", Área2);
 
-    printf("PIB: R$ %f\n", PIB2);
+    printf("PIB: R$ %lf\n", PIB2);
     
     printf("Número de pontos turísticos: %d\n", Turismo2);
+
+    printf("Densidade populacional: %lf\n", Densidade2);
+
+    printf("PIB per capita: %lf\n\n", PIBCapita2);
   
 return 0;
 } 
